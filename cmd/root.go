d4d6b185939697f014df67772421512f22539413
Juliano Martinez <juliano.martinez@adyen.com>
2023-11-27 13:45:20 +0100

    adds commands to manage the sync

--- cmd/root.go
+++ cmd/root.go
@@ -28,16 +28,7 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "bedel",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "Small utility to sync redis acls with a master instance",
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -51,16 +42,13 @@ func Execute() {
 
 func init() {
 	cobra.OnInitialize(initConfig)
-
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.bedel.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.PersistentFlags().StringP("address", "a", "", "address of the slave to manage instance eg: 127.0.0.1:6379")
+	viper.BindPFlag("address", rootCmd.PersistentFlags().Lookup("address"))
+	rootCmd.PersistentFlags().StringP("password", "p", "", "password to manage acls")
+	viper.BindPFlag("password", rootCmd.PersistentFlags().Lookup("password"))
+	rootCmd.PersistentFlags().StringP("username", "u", "", "username to manage acls")
+	viper.BindPFlag("username", rootCmd.PersistentFlags().Lookup("username"))
 }
 
 // initConfig reads in config file and ENV variables if set.
@@ -79,10 +67,25 @@ func initConfig() {
 		viper.SetConfigName(".bedel")
 	}
 
-	viper.AutomaticEnv() // read in environment variables that match
+	viper.SetDefault("syncInterval", 10)
+	viper.AutomaticEnv()
 
-	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
 		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	}
+
+	if !viper.IsSet("address") {
+		fmt.Fprintln(os.Stderr, "address is required")
+		os.Exit(1)
+	}
+
+	if !viper.IsSet("password") {
+		fmt.Fprintln(os.Stderr, "password is required")
+		os.Exit(1)
+	}
+
+	if !viper.IsSet("username") {
+		fmt.Fprintln(os.Stderr, "username is required")
+		os.Exit(1)
 	}
 }
